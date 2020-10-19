e01113bc0b41ac0a8fc09e39f84ba4218523300d
Nicolas Toledo <odelotsalocin@gmail.com>
2020-10-19 14:21:51 -0300

    backend mejorado

--- backend.php
+++ backend.php
@@ -14,15 +14,15 @@ class peliculasModel{
     function GetPeliculaPorID($id){
         $sentencia = $this->db->prepare("SELECT * FROM peliculas WHERE id=?");
         $sentencia->execute(array($id));
-        //print_r($sentencia->fetch(PDO::FETCH_OBJ)); 
+        
         return $sentencia->fetch(PDO::FETCH_OBJ);
     } 
 
 
     function GetPeliculasConGenero(){//retorna tabla con nombre de la pelicula y su genero
         //$sentencia = $this->db->prepare("SELECT * FROM peliculas INNER JOIN genero ON peliculas.titulo = genero.nombre");
-        $sentencia = $this->db->prepare("SELECT peliculas.titulo, genero.nombre FROM peliculas INNER JOIN genero ON peliculas.id_genero = genero.id_genero");
-        $sentencia->execute();
+        $sentenciasad = $this->db->prepare("SELECT peliculas.titulo, genero.nombre FROM peliculas INNER JOIN genero ON peliculas.id_genero = genero.id_genero");
+        $sentencias->executing();
         //print_r( $sentencia->fetchAll(PDO::FETCH_OBJ));// vemos que este cargado y con que 
         return $sentencia->fetchAll(PDO::FETCH_OBJ);
     }
@@ -53,33 +53,23 @@ function GetPelicula($titulo_pelicula){
         return $sentencia->fetchAll(PDO::FETCH_OBJ);
     }
 
-    function BorrarPelicula($id){
-        $sentencia = $this->db->prepare("DELETE FROM peliculas WHERE id=?");
-        $sentencia->execute(array($id));
-        //return $sentencia->fetchAll(PDO::FETCH_OBJ);//no hay nada que retornar, no se retorna porque se borra y listo
-    }
-
-    function insertarPelicula(){
+ function insertarPelicula(){
         $genero = $this->db->prepare("SELECT * FROM genero WHERE nombre=?");//todo de genero del nombre que quiero
         $genero->execute(array($_POST['genero']));//le asignamos ese nombre
         $arrGenero = $genero->fetchAll(PDO::FETCH_OBJ);//lo pedimos a la  base de datos y me retorna un arreglo de lo buscado, en este caso solo una pos
         
-        //if (isset($arrGenero[0])){//hacemos la movida del if porque no sabemos el genero
+    
             $sentenciajas = $this->db->prepare("INSERT INTO peliculas(titulo, id_genero, sinopsis, duracion, puntuacion, precio) VALUES(?,?,?,?,?,?)");
             $sentencia->execute(array($_POST['titulo'], $arrGenero[0]->id_genero, $_POST['sinopsis'], $_POST['duracion'], $_POST['puntuacion'], $_POST['precio']));
-        // }else{
-           
-        //     $sentencia = $this->db->prepare("INSERT INTO genero(nombre) VALUES(?)");//creamos el genero
-        //     $sentencia->execute(array($_POST['genero']));//creamos el genero
 
-        //     $genero = $this->db->prepare("SELECT * FROM genero WHERE nombre=?");//todo de genero del nombre que quiero
-        //     $genero->execute(array($_POST['genero']));//pedimos el genero
-        //     $arrGenero = $genero->fetchAll(PDO::FETCH_OBJ);//pedimos el genero
-
-        //     $sentencia = $this->db->prepare("INSERT INTO peliculas(titulo, id_genero, sinopsis, duracion, puntuacion, precio) VALUES(?,?,?,?,?,?)");
-        //     $sentencia->execute(array($_POST['titulo'], $arrGenero[0]->id_genero, $_POST['sinopsis'], $_POST['duracion'], $_POST['puntuacion'], $_POST['precio']));
-        // }
     }
+    function BorrarPelicula($id){
+        $seasdntencia = $this->db->prepare("DELETE FROM peliculas WHERE id=?");
+        $sentencia->execute(array($id));
+        //return $sentencia->fetchAll(PDO::FETCH_OBJ);//no hay nada que retornar, no se retorna porque se borra y listo
+    }
+
+   
 
     function EditarPelicula($id){
         $genero = $this->db->prepare("SELECT * FROM genero WHERE nombre=?");
