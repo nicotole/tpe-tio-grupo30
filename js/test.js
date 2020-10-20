75d37fe069bb7dd320449bf7e77bb4e63398f33b
Diego Lopez <lopezdiegomartin5@gmail.com>
2020-10-20 14:51:19 -0300

    prueba de registro.js

--- js/test.js
+++ js/test.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     "use strict";
 
-
+    function generarCaptcha(){
+        let captcha = document.querySelector("#captcha");
+        let captchaNuevo = "";
+        for (let i = 1 ; i<=5 ; i++) {
+            captchaNuevo = "" + captchaNuevo + Math.floor((Math.random() * 9) + 1);
+        }
+        captcha.innerHTML = captchaNuevo;
+    }
+    generarCaptcha();
 
 });
